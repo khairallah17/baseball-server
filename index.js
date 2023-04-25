2360b28959f144c651f9ad53e5f80576e787ec5d
khairallah17 <med.khairallah3@gmail.com>
2023-04-25 05:27:43 +0100

    updates limits

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const cors = require("cors")
 const pitchers = require("./models/Pitchers")
 const limit = require("express-rate-limit")
 
-// const limiter = limit({
-//     windowMs: 24 * 60 * 60 * 1000,
-//     max: 3,
-//     message: 'you reached the limit per day see you next time :)'
-// })
+const limiter = limit({
+    windowMs: 24 * 60 * 60 * 1000,
+    max: 3,
+    message: 'you reached the limit per day see you next time :)'
+})
 
 require("dotenv").config()
 
@@ -24,66 +24,67 @@ app.use(cors({
     origin: "*",
 }))
 
-app.get("/", (req, res) => {
+app.get("/",limiter, (req, res) => {
 
+    console.log("hello")
     res.send("API WORKING")
 
 })
 
-app.post("/hitters", async (req, res) => {
-    const { name } = req.body
+// app.post("/hitters", async (req, res) => {
+//     const { name } = req.body
 
-    console.log(req.body)
+//     console.log(req.body)
 
-    try {
+//     try {
 
-        const data = await hitters.findOne({ Name: name },{Name:1,Team:1,AVG:1,OBP:1,SLG:1,"BB%":1,"K%":1})
+//         const data = await hitters.findOne({ Name: name },{Name:1,Team:1,AVG:1,OBP:1,SLG:1,"BB%":1,"K%":1})
 
-        res.json(data)
-    } catch (err) {
-        console.log(err.message)
-    }
-})
+//         res.json(data)
+//     } catch (err) {
+//         console.log(err.message)
+//     }
+// })
 
-app.get("/hitters", async (req, res) => {
+// app.get("/hitters", async (req, res) => {
 
-    const result = await hitters.find({},{Name:1})
+//     const result = await hitters.find({},{Name:1})
 
-    console.log("here")
+//     console.log("here")
 
-    res.json(result)
+//     res.json(result)
 
-})
+// })
 
-app.get("/pitchers", async (req, res) => {
+// app.get("/pitchers", async (req, res) => {
 
-    const result = await pitchers.find({}, {Name: 1})
+//     const result = await pitchers.find({}, {Name: 1})
 
-    console.log("here")
+//     console.log("here")
 
-    res.json(result)
+//     res.json(result)
 
-})
+// })
 
-app.post("/pitchers", async (req, res) => {
+// app.post("/pitchers", async (req, res) => {
 
-    const { name } = req.body
+//     const { name } = req.body
 
-    console.log(name)
+//     console.log(name)
 
-    try {
+//     try {
 
-        const result = await pitchers.find({Name: name}, {Name:1,Team:1,ERA:1,WHIP:1,"K/9":1})
+//         const result = await pitchers.find({Name: name}, {Name:1,Team:1,ERA:1,WHIP:1,"K/9":1})
 
-        res.json(result)
+//         res.json(result)
 
-    } catch (err) {
-        console.log(err.message)
-    }
+//     } catch (err) {
+//         console.log(err.message)
+//     }
 
-})
+// })
 
-mongoose.connection.once('open', () => {
-    console.log("connected to mongodb")
+// mongoose.connection.once('open', () => {
+//     console.log("connected to mongodb")
     app.listen(port, () => console.log(`listening on port ${port}`))
-})
+// })
